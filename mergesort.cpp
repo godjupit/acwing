b73c497e386d889ae98923990f6f207e03f47f97
godjupit <1367258057@qq.com>
2025-06-27 01:16:42 +0800

    123

--- mergesort.cpp
+++ mergesort.cpp
@@ -6,28 +6,23 @@ int a[N];
 int temp[N];
 
 void merge_sort(int a[],int l, int r){
-    if(l >= r){
-        return;
-    }
+   if(l >= r) return;
+   
+   int mid = (l + r) / 2;
+   int i = l; int j = mid + 1;
+   merge_sort(a,l,mid);
+   merge_sort(a,mid+1,r);   
+   int k = 0;
+   while(i <= mid && j <= r){
+        if(a[i] < a[j]) temp[k++] = a[i++];
+        else temp[k++] = a[j++];
+   }
+   while(i <= mid) temp[k++] = a[i++];
+   while(j <= r) temp[k++] = a[j++];
 
-    int mid = (l + r) / 2;
-
-    merge_sort(a, l, mid);
-    merge_sort(a, mid+1, r);
-
-    int i = l;
-    int j = mid + 1;
-    int k = 0;
-    while(i <= mid && j <= r){
-        if(a[i] < a[j]){
-            temp[k++] = a[i++];
-        }else{
-            temp[k++] = a[j++];
-        }
-    }
-    while(i <= mid) temp[k++] = a[i++];
-    while(j <= r) temp[k++] = a[j++];
-    for(i = l, j = 0;i<=r;i++,j++) a[i] = temp[j];
+   for(i = l,j = 0; i <= r; i++,j++){
+        a[i] = temp[j];
+   }
 
 }
 
