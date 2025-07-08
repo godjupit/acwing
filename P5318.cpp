bc13b4256ac2dc4b2284666d6642621a5725551c
godjupit <1367258057@qq.com>
2025-07-08 18:12:25 +0800

    123

---
+++ P5318.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+using namespace std;
+
+const int N = 1e6+10,M = N * 2;
+int n, m;
+int h[N],e[M],me[M],idx;
+
+void add(int a, int b){
+    
+}
+
+int main(){
+    memset(h,-1,sizeof(h));
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        int num1;
+        int num2;
+        cin >> num1 >> num2;
+        add(num1, num2);
+
+    }
+
+}
